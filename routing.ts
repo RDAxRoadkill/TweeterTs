033219dde0e8e4bd3002dca6a7ceebd03b456426
Rowin van Blokland <rowinvanblokland@hotmail.nl>
2019-02-12 16:03:06 +0100

    Basic register and login templates

--- routing.ts
+++ routing.ts
@@ -6,5 +6,21 @@ router.route('/')
         console.log("First");
     });
 
+router.route('/register')
+    .get(function (req: any, res: any) {
+        res.sendFile(__dirname + '/templates/register.html');
+    })
+    .post(function (req: any, res: any) {
+
+    });
+
+router.route('/login')
+    .get(function (req: any, res: any) {
+        res.sendFile(__dirname + '/templates/login.html');
+    })
+    .post(function (req: any, res: any) {
+
+    });
+
 
 export default router;
--- server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var routing_1 = require("./routing");
-app.use(routing_1["default"]);
-app.listen(process.env.PORT || 8000);
-console.log("Server up");
