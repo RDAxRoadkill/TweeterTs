b663d5f7ccce909d3ad6f393288b693737203231
Wouter Jansen <wouter97@planet.nl>
2019-02-12 15:44:49 +0100

    Added mongoose connection

--- server.ts
+++ server.ts
@@ -1,9 +1,29 @@
 import express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 import routing from './routing';
 
+mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+//local db
+const mongoUrl = 'mongodb://localhost/tweeter'
+
+mongoose.connect(mongoUrl, {
+	useNewUrlParser: true,
+	// auth: {
+	//  	user: process.env.dbUsername,
+	//  	password: process.env.pwd
+	// }
+});
+
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
+conn.once('open', () => {
+	console.log('MongoDB connected.')
+});
+
 app.use(routing);
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8000); 
 
 
 console.log("Server up");
